20e3c1d53d8ab0aa969a0adc3d69cae54eab24a8
Amnon Catav <amnoncatav1@gmail.com>
2023-12-07 13:58:11 +0200

    Update test_cohere_encoder.py

--- tests/unit/test_cohere_encoder.py
+++ tests/unit/test_cohere_encoder.py
@@ -1,15 +1,11 @@
 import pytest
 from unittest.mock import patch, Mock
-from typing import List
 from pinecone_text.dense import CohereEncoder
-
-
-real_encoder = CohereEncoder(check_api_key=False)
 
 
 def create_mock_response(embeddings):
     mock_response = Mock()
-    mock_response.data = [Mock(embedding=embedding) for embedding in embeddings]
+    mock_response.embeddings = [embedding for embedding in embeddings]
     return mock_response
 
 
@@ -23,26 +19,20 @@ def cohere_encoder():
         yield CohereEncoder()
 
 
-@pytest.fixture
-def real_encoder():
-    yield CohereEncoder()
-
-
 def test_init_without_cohere_installed():
     with patch("pinecone_text.dense.cohere_encoder._cohere_installed", False):
         with pytest.raises(ImportError):
-            CohereEncoder()
+            CohereEncoder(check_api_key=False)
 
 
 def test_init_with_kwargs():
-    with patch("pinecone_text.dense.cohere_encoder.cohere.Client") as mock_cohere:
+    with patch("pinecone_text.dense.cohere_encoder.cohere") as mock_cohere:
         CohereEncoder(
-            api_key="test_api_key", organization="test_organization", timeout=30
-        )
-        mock_cohere.assert_called_with(
             api_key="test_api_key",
-            organization="test_organization",
-            timeout=30,
+            check_api_key=False,
+        )
+        mock_cohere.Client.assert_called_with(
+            api_key="test_api_key", check_api_key=False
         )
 
 
@@ -58,12 +48,11 @@ def encode_by_type(cohere_encoder, encoding_function, test_input):
         ("encode_queries"),
     ],
 )
-def test_encode_single_text(real_encoder, encoding_function):
-    if encoding_function == "encode_documents":
-        out = real_encoder.encode_documents("test text")
-    else:
-        out = real_encoder.encode_queries("test text")
-    assert isinstance(out, list)
+def test_encode_single_text(cohere_encoder, encoding_function):
+    with patch.object(cohere_encoder._client, "embed", create=True) as mock_embeddings:
+        mock_embeddings.return_value = mock_single_embedding
+        result = encode_by_type(cohere_encoder, encoding_function, "test text")
+        assert result == [0.1, 0.2, 0.3]
 
 
 @pytest.mark.parametrize(
@@ -73,13 +62,11 @@ def test_encode_single_text(real_encoder, encoding_function):
         ("encode_queries"),
     ],
 )
-def test_encode_multiple_texts(real_encoder, encoding_function):
-    if encoding_function == "encode_documents":
-        out = real_encoder.encode_documents(["test text", "doc 2"])
-    else:
-        out = real_encoder.encode_queries(["test text", "doc 2"])
-    assert isinstance(out, list)
-    assert isinstance(out[0], list)
+def test_encode_multiple_texts(cohere_encoder, encoding_function):
+    with patch.object(cohere_encoder._client, "embed", create=True) as mock_embeddings:
+        mock_embeddings.return_value = mock_multiple_embeddings
+        result = encode_by_type(cohere_encoder, encoding_function, ["text1", "text2"])
+        assert result == [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]]
 
 
 @pytest.mark.parametrize(
@@ -94,6 +81,15 @@ def test_encode_invalid_input(cohere_encoder, encoding_function):
         encode_by_type(cohere_encoder, encoding_function, 123)
 
 
-def test_invalid_model():
-    with pytest.raises(ValueError):
-        CohereEncoder(model_name="not-supported-model")
+@pytest.mark.parametrize(
+    "encoding_function",
+    [
+        ("encode_documents"),
+        ("encode_queries"),
+    ],
+)
+def test_encode_error_handling(cohere_encoder, encoding_function):
+    with patch.object(cohere_encoder._client, "embed", create=True) as mock_embeddings:
+        mock_embeddings.side_effect = ValueError("OpenAI API error")
+        with pytest.raises(ValueError, match="OpenAI API error"):
+            encode_by_type(cohere_encoder, encoding_function, "test text")
